c53d258af69d07b7194d9af62185648674d57166
Ed <mail@544062.ru>
2023-06-17 18:28:11 +0300

    feat: implement read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import { readFile } from "fs/promises";
+
+const getAbsolutePath = (path) => {
+  return new URL(path, import.meta.url);
+};
+
+const filesFolder = "files";
+const fileName = "fileToRead.txt";
+const filePath = getAbsolutePath(`${filesFolder}/${fileName}`);
+const encoding = "utf-8";
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const fileData = await readFile(filePath, encoding);
+    console.log(fileData);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
