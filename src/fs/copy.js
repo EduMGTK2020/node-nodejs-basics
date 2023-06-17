f9cce822fbbcfc8433eb3c9b610ec489d33944f1
Ed <mail@544062.ru>
2023-06-17 17:42:40 +0300

    fix: run prettier

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,38 +1,39 @@
-import { readdir, stat, mkdir, copyFile } from 'fs/promises';
+import { readdir, stat, mkdir, copyFile } from "fs/promises";
 
-const filesFolder = 'files';
-const filesCopyFolder = 'files_copy';
+const filesFolder = "files";
+const filesCopyFolder = "files_copy";
 
 const pathExist = async (path) => {
-    try {
-        await stat(path);
-        return true;
-    }
-    catch {
-        return false;
-    }
-}
+  try {
+    await stat(path);
+    return true;
+  } catch {
+    return false;
+  }
+};
 
 const getAbsolutePath = (path) => {
-    return new URL(path, import.meta.url);
-}
+  return new URL(path, import.meta.url);
+};
 
 const copy = async () => {
-    const filesOriginPath = getAbsolutePath(filesFolder);
-    const filesCopyPath = getAbsolutePath(filesCopyFolder);
+  const filesOriginPath = getAbsolutePath(filesFolder);
+  const filesCopyPath = getAbsolutePath(filesCopyFolder);
 
-    if (!(await pathExist(filesOriginPath)) || await pathExist(filesCopyPath)) {
-        throw new Error('FS operation failed');
-    }
-    else {
-        const [filesNameToCopy] = await Promise.all([readdir(filesOriginPath), mkdir(filesCopyPath)]);
-        const copyPromises = filesNameToCopy.map((name) =>{
-            const fileFrom =  getAbsolutePath(`${filesOriginPath}/${name}`);  
-            const fileTo = getAbsolutePath(`${filesCopyPath}/${name}`);
-            return copyFile(fileFrom, fileTo); 
-        });
-        await Promise.all(copyPromises);
-    }
+  if (!(await pathExist(filesOriginPath)) || (await pathExist(filesCopyPath))) {
+    throw new Error("FS operation failed");
+  } else {
+    const [filesNameToCopy] = await Promise.all([
+      readdir(filesOriginPath),
+      mkdir(filesCopyPath),
+    ]);
+    const copyPromises = filesNameToCopy.map((name) => {
+      const fileFrom = getAbsolutePath(`${filesOriginPath}/${name}`);
+      const fileTo = getAbsolutePath(`${filesCopyPath}/${name}`);
+      return copyFile(fileFrom, fileTo);
+    });
+    await Promise.all(copyPromises);
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,17 +1,16 @@
-import { writeFile } from 'fs/promises';
+import { writeFile } from "fs/promises";
 
-const fileName = 'fresh.txt';
-const fileContent = 'I am fresh and young';
-const filesFolder = 'files';
+const fileName = "fresh.txt";
+const fileContent = "I am fresh and young";
+const filesFolder = "files";
 
 const create = async () => {
-    const filePath = new URL(`${filesFolder}/${fileName}`, import.meta.url);
-    try {
-        await writeFile(filePath, fileContent, { flag: 'wx' });  
-    } 
-    catch(err) {
-        throw new Error('FS operation failed');
-    }
+  const filePath = new URL(`${filesFolder}/${fileName}`, import.meta.url);
+  try {
+    await writeFile(filePath, fileContent, { flag: "wx" });
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
