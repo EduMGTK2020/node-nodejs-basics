e846d8318e23bc55dcc71ddac525cfd29b257c07
Ed <mail@544062.ru>
2023-06-17 18:20:40 +0300

    feat: implement list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { readdir } from "fs/promises";
+
+const getAbsolutePath = (path) => {
+  return new URL(path, import.meta.url);
+};
+
+const filesFolder = "files";
+const fileFolderPath = getAbsolutePath(filesFolder);
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const listFiles = await readdir(fileFolderPath);
+    console.log(listFiles);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
