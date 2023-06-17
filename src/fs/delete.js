1daf5059c6d0d66ee06a067680599188539fd6e6
Ed <mail@544062.ru>
2023-06-17 18:13:03 +0300

    feat: implement delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { rm as removeFile } from "fs/promises";
+
+const getAbsolutePath = (path) => {
+  return new URL(path, import.meta.url);
+};
+
+const filesFolder = "files";
+const fileToRemove = "fileToRemove.txt";
+const fileToRemovePath = getAbsolutePath(`${filesFolder}/${fileToRemove}`);
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await removeFile(fileToRemovePath);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await remove();
