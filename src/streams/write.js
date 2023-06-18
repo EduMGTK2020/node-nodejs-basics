a5ef22c7e154c3a986aa82f17457589ba52f943f
Ed <mail@544062.ru>
2023-06-18 11:43:22 +0300

    feat: implement streams/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
+const getAbsolutePath = (path) => {
+  return new URL(path, import.meta.url);
+};
+
+const filesFolder = 'files';
+const file = 'fileToWrite.txt';
+const filePath = getAbsolutePath(`${filesFolder}/${file}`);
+
 const write = async () => {
-    // Write your code here 
+  const writeStream = createWriteStream(filePath);
+  await pipeline(process.stdin, writeStream);
 };
 
 await write();
