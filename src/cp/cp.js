a0e0e035ff67e6f851a034daf68c32b765820a4f
Ed <mail@544062.ru>
2023-06-18 19:34:36 +0300

    refactor: add second implementation for cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -8,7 +8,13 @@ const fileChildProcessPath = new URL(
 );
 
 const spawnChildProcess = async (args) => {
-  fork(fileChildProcessPath, args);
+  // first variant
+  const childProcess = fork(fileChildProcessPath, args, { silent: true });
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
+
+  // or the second implementation in one line if you don't like the first one :)
+  // fork(fileChildProcessPath, args);
 };
 
 // Put your arguments in function call to test this functionality
