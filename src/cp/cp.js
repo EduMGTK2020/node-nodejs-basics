431e4933b4f4f1842541201a66f9b100463ffe42
Ed <mail@544062.ru>
2023-06-18 13:59:17 +0300

    feat: implement cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { fork } from 'child_process';
+
+const getAbsolutePath = (path) => {
+  return new URL(path, import.meta.url);
+};
+
+const filesFolder = 'files';
+const fileChildProcess = 'script.js';
+const fileChildProcessPath = getAbsolutePath(
+  `${filesFolder}/${fileChildProcess}`
+);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  fork(fileChildProcessPath, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2', 'arg3']);
