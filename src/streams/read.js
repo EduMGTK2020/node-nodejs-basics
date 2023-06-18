ad2097b34ea5c6a7c53593bac49f1454bd96a5e8
Ed <mail@544062.ru>
2023-06-18 11:30:58 +0300

    feat: implement streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
+const getAbsolutePath = (path) => {
+  return new URL(path, import.meta.url);
+};
+
+const filesFolder = 'files';
+const file = 'fileToRead.txt';
+const filePath = getAbsolutePath(`${filesFolder}/${file}`);
+
 const read = async () => {
-    // Write your code here 
+  const readStream = createReadStream(filePath);
+  await pipeline(readStream, process.stdout);
 };
 
 await read();
