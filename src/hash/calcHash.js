62e6e0f37a9a871f050ce5ba25fa7cf3e9244f1f
Ed <mail@544062.ru>
2023-06-18 11:13:41 +0300

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { readFile } from 'fs/promises';
+import { createHash } from 'crypto';
+
+const getAbsolutePath = (path) => {
+  return new URL(path, import.meta.url);
+};
+
+const filesFolder = 'files';
+const fileToHash = 'fileToCalculateHashFor.txt';
+const fileToHashPath = getAbsolutePath(`${filesFolder}/${fileToHash}`);
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const data = await readFile(fileToHashPath);
+    const hash = createHash('sha256').update(data);
+    const hex = hash.digest('hex');
+    console.log(hex);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await calculateHash();
